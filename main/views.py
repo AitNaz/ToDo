3d1c3d055bf60302dd3f2b5755e3984b4bf77943
AitNaz <aitbaeva.nk@gmail.com>
2021-01-24 19:30:41 +0600

    comment

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return HttpResponse("hello world")
+
+def test(request):
+    return render(request, "test.html")
+
+def go(request):
+    return render(request, "go.html")
