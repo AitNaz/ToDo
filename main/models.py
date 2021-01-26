37bf4d42df2cc46981fb1e4025c60acecd643915
AitNaz <aitbaeva.nk@gmail.com>
2021-01-26 14:01:59 +0600

    added to do list in test page, edit class, edit syntaxys mistakes

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ToDo(models.Model):
     text = models.CharField(max_length=100)
-    creates_ed = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
 
